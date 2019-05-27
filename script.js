da204636fe9ac2cb61f5ad8befecde0136f3ee2d
rebeccanylin <rebeccanylin@hotmail.com>
2019-05-27 12:13:26 +0200

    input lavet til login - med stylet bokse

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", sidenVises);
+
 function sidenVises() {
     console.log(sidenVises);
 
@@ -6,10 +8,27 @@ function sidenVises() {
 
     document.querySelector("#forside").classList.add("hide", forside);
     document.querySelector("#login").removeEventListener("click", forside);
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
 function forside() {
     console.log(forside);
 
     document.querySelector("#forside").classList.remove("hide", forside);
+
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
